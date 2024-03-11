fbfff5262a9a822822addffe43433ca8702cf955
fawazalzamel <fawazzamel1@gmail.com>
2024-03-11 12:47:06 +0300

    StarUp code

---
+++ FuelAndServices/NetworkClass/NetworkManger.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManger.swift
+//  FuelAndServices
+//
+//  Created by Fawaz Alzamel on 11/03/2024.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManger{
+    
+    private let baseURL = ""
+    
+    static let shared = NetworkManger()
+    
+    
+}
